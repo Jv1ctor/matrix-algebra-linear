8c2af5bcd6ad7f6b4d8039e5dda751fe97a1d9aa
KauamSobreira <moraiskauam6@gmail.com>
2025-03-19 16:28:25 -0300

    Add files via upload
    Vetores implementados tanto para transpor quanto para somar

--- src/LinearAlgebra.java
+++ src/LinearAlgebra.java
@@ -2,7 +2,7 @@ public class LinearAlgebra {
 
     // Metodo para transpor uma matriz. A transposição troca as linhas pelas colunas.
     public static Matrix transpose(Matrix a) {
-        Matrix result = new Matrix(a.getColumns(), a.getRows(), new double [a.getColumns() * a.getRows()]);
+        Matrix result = new Matrix(a.getColumns(), a.getRows(), new double[a.getColumns() * a.getRows()]);
 
         // Percorre a matriz e troca os índices para fazer a transposição
         for (int i = 0; i < a.getRows(); i++) {
@@ -12,11 +12,11 @@ public class LinearAlgebra {
         }
         return result;
     }
-//
-//    public static Vector transpose(Vector a) {
-//        return new Vector(a.getDim(), a.getElements());
-//    }
-//
+
+    public static Vector transpose(Vector a) {
+        return new Vector(a.getDim(), a.getElements());
+    }
+
 
     // Metodo para somar duas matrizes.
     public static Matrix sum(Matrix a, Matrix b) {
@@ -25,7 +25,7 @@ public class LinearAlgebra {
             return null; // Ou qualquer outro valor que indique erro
         }
 
-        Matrix result = new Matrix(a.getRows(), a.getColumns(), new double[a.getRows() * a.getColumns()]);
+        Matrix result = new Matrix(a.getRows(), a.getColumns(), new double[a.getColumns() * a.getRows()]);
 
         // Percorre cada elemento da matriz e soma os valores correspondentes
         for (int i = 0; i < a.getRows(); i++) {
@@ -34,5 +34,24 @@ public class LinearAlgebra {
             }
         }
         return result;
+    }
+
+    // Metodo para somar dois vetores
+    public static Vector sum(Vector a, Vector b) {
+        if (a.getDim() != b.getDim()) {
+            System.out.println("Erro: Os vetores devem ter o mesmo tamanho.");
+            return null; // Ou qualquer outro valor que indique erro
+        }
+
+        // Cria um novo vetor para armazenar o resultado
+        double[] resultElements = new double[a.getDim()];
+
+        // Percorre os vetores e soma os elementos correspondentes
+        for (int i = 0; i < a.getDim(); i++) {
+            resultElements[i] = a.getElements()[i] + b.getElements()[i];
+        }
+
+        // Cria e retorna o vetor resultante
+        return new Vector(a.getDim(), resultElements);
     }
 }
