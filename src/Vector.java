c8dc87b84bd3a9652c604321019f920aa3c66aa4
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-03-19 11:44:28 -0300

    alverificação de dimensão, tipo de dados para double(maior precisão).

--- src/Vector.java
+++ src/Vector.java
@@ -1,33 +1,26 @@
 import java.util.Arrays;
 public class Vector {
     private int dim;
-    private int[] elements;
+    private double[] elements;
 
-    public Vector(int dim, int[] elements) {
+    public Vector(int dim, double[] elements) {
+        if(dim != elements.length){
+            throw new Error("Quantidade de elementos incompatível com as dimensões do vetor");
+        }
+
         this.dim = dim;
         this.elements = elements;
     }
 
-    public int get(int i) {
+    public double get(int i) {
         return elements[i];
     }
 
-    public void set(int i, int value) {
+    public void set(int i, double value) {
         elements[i] = value;
     }
 
-    public void printVector() {
+    public void show() {
         System.out.println(Arrays.toString(elements));
     } // esse método mostra o vetor completo
-
-    public static void main(String[] args) {
-        Vector vetor = new Vector(3, new int[]{1, 2, 3});
-
-        System.out.println("Valor no índice 1: " + vetor.get(1)); // vai mostrar o valor que tem no indice 1
-        vetor.set(1, 10); // altera o valor do indice 1 por 10
-        System.out.println("Novo valor no índice 1: " + vetor.get(1));
-
-        System.out.print("Vetor completo: ");
-        vetor.printVector(); //vai retornar o vetor com a mudança de valor
-    }
 }
