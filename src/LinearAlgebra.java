069089220110581bcb19d51ceca4164d6243a96e
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-03-19 22:08:36 -0300

    implementado o metodo gauss

--- src/LinearAlgebra.java
+++ src/LinearAlgebra.java
@@ -35,4 +35,49 @@ public class LinearAlgebra {
         }
         return result;
     }
+
+    public static Matrix gauss(Matrix a){
+        int rows = a.getRows();
+        int cols = a.getColumns();
+        Matrix result = new Matrix(a.getRows(), a.getColumns(), new double[rows * cols]);
+        for (int i = 0; i < a.getRows(); i++) {
+            for (int j = 0; j < a.getColumns(); j++) {
+                result.set(i, j, a.get(i, j));
+            }
+        }
+
+
+        for (int j = 0; j < rows; j++) {
+            int maxRow = j;
+            for (int i = j + 1; i < rows; i++) {
+                if (Math.abs(result.get(i, j)) > Math.abs(result.get(maxRow, j))) {
+                    maxRow = i;
+                }
+            }
+
+            // Troca as linhas j e maxRow
+            if (maxRow != j) {
+                for (int c = 0; c < cols; c++) {
+                    double temp = result.get(j, c);
+                    result.set(j, c, result.get(maxRow, c));
+                    result.set(maxRow, c, temp);
+                }
+            }
+
+            if (result.get(j, j) == 0) {
+                continue;
+            }
+
+            for (int i = j + 1; i < rows; i++){
+                double k = - result.get(i,j) / result.get(j, j); // k = -A(i, j) / A(j, j)
+                for (int c = j; c < result.getColumns(); c++){
+                    double elementLine = result.get(i, c);
+                    result.set(i, c, elementLine + k * result.get(j, c));
+                }
+            }
+        }
+        return result;
+    }
+
+
 }
