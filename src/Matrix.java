53073a8643c388967fa4b757ed844bcfde1828b1
Jv1ctor <j.rodriguesbarretos@gmail.com>
2025-03-23 19:20:38 -0300

    trabalhando apenas com arrays unidimensionais

--- src/Matrix.java
+++ src/Matrix.java
@@ -4,45 +4,24 @@ public class Matrix {
     private int rows;
     private int columns;
     private double[] elements;
-    private int indexElements = 0;
-    protected double[][] matrixElements;
 
     Matrix(int rows, int columns, double[] elements){
+        if(elements.length != (rows * columns)){
+            throw new IllegalArgumentException("Quantidade de elementos incompatível com as dimensões da matriz");
+        }
         this.rows = rows;
         this.columns = columns;
-        if(elements.length <= (rows * columns)){
-            this.elements = elements;
-        }else{
-            System.out.println(rows * columns);
-            System.out.println(elements.length);
-            throw new Error("Quantidade de elementos incompatível com as dimensões da matriz");
-        }
-
-        this.matrixElements = this.generateArrayMatrix();
+        this.elements = elements;
     }
-
-    private double[][] generateArrayMatrix(){
-        double[][] array = new double[this.rows][];
-        for(int i = 0; i < this.rows; i++){
-            double[] columnsElements = new double[this.columns];
-            for(int j = 0; j < this.columns; j++){
-                columnsElements[j] = elements[indexElements];
-                indexElements++;
-            }
-
-            array[i] = columnsElements;
-        }
-
-        return  array;
-    }
-
 
     public double get(int i, int j){
-        return this.matrixElements[i][j];
+        int index = i * columns + j;
+        return this.elements[index];
     }
 
     public void set(int i, int j, double value){
-        this.matrixElements[i][j] = value;
+        int index = i * columns + j;
+        this.elements[index] = value;
     }
 
     public int getRows() {
@@ -54,13 +33,13 @@ public class Matrix {
     }
 
     public void show(){
-        for(int i = 0; i < rows; i++){
-            for (int j= 0; j < columns; j++){
-                System.out.print(matrixElements[i][j] + "  ");
 
-                if(j == columns - 1){
-                    System.out.print("\n");
-                }
+        for(int i = 0; i < rows * columns; i ++){
+            int indexCols = i % columns;
+            System.out.print(elements[i] + "  ");
+
+            if(indexCols == columns - 1){
+                System.out.print("\n");
             }
         }
     }
